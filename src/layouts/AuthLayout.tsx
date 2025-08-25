2989d009462168cb114cde6ee7c64bad462f1d38
thanhlv <vinhthanhluong@gmail.com>
2025-08-25 21:15:36 +0700

    update setLoginPage

--- src/layouts/AuthLayout.tsx
+++ src/layouts/AuthLayout.tsx
@@ -1,6 +1,12 @@
-import { Outlet } from 'react-router-dom'
+import { Navigate, Outlet } from 'react-router-dom'
+import { useAuthStore } from '@/store/auth.store'
 
 export default function AuthLayout() {
+  const { user } = useAuthStore();
+  if (user) {
+    return <Navigate to={user?.user.role === 'USER' ? '/' : '/dashboard'} />
+  }
+
   return (
     <div className='p-10'>
       <Outlet />
--- src/pages/HomeTemplate/_components/Header/index.tsx
+++ src/pages/HomeTemplate/_components/Header/index.tsx
@@ -1,7 +1,19 @@
-import React from 'react'
+import { useNavigate } from 'react-router-dom';
+
+import { useAuthStore } from '@/store/auth.store';
+import { Button } from '@/components/ui/button';
 
 export default function Header() {
+  const { clearUser } = useAuthStore();
+  const navigate = useNavigate()
+  const handleLogout = () => {
+    clearUser();
+    navigate('/auth/login')
+  }
   return (
-    <div>Header</div>
+    <div>
+      Header Home
+      <Button onClick={handleLogout}>Đăng xuất</Button>
+    </div>
   )
 }
