e1af526524efe37ce6eead489b85416520a20c51
thanhlv <vinhthanhluong@gmail.com>
2025-08-26 00:11:07 +0700

    update sidebar admin

--- src/pages/AdminTemplate/_components/Header/index.tsx
+++ src/pages/AdminTemplate/_components/Header/index.tsx
@@ -10,9 +10,25 @@ export default function Header() {
     navigate('/auth/login')
   }
   return (
-    <div>
-      Header Dashboard
-      <Button onClick={handleLogout}>Đăng xuất</Button>
+    <div className='fixed top-0 inset-x-0 h-20 shadow-2xs flex justify-between items-center px-5'>
+      <div className='logo'>
+        <p className='text-4xl font-bold'>LaniBnB</p>
+      </div>
+
+      <div className='avatar relative'>
+        <div className='flex items-center gap-3'>
+          <img className="w-10 h-10 rounded-full cursor-pointer" src="https://flowbite.com/docs/images/people/profile-picture-5.jpg" alt="User dropdown" />
+          <div className='font-medium'>Bonnie Green</div>
+        </div>
+        <div className="absolute top-[120%] overflow-hidden right-0 z-10 bg-white divide-y divide-gray-200 rounded-md shadow-sm w-44 dark:bg-gray-700 dark:divide-gray-600">
+          <ul className="text-sm text-gray-700 dark:text-gray-200">
+            <li>
+              <a href="#" className="block px-4 py-3 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Thông tin</a>
+            </li>
+          </ul>
+          <p onClick={handleLogout} className="block px-4 py-3 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Đăng xuất</p>
+        </div>
+      </div>
     </div>
   )
 }
--- src/pages/AdminTemplate/_components/Sidebar/index.tsx
+++ src/pages/AdminTemplate/_components/Sidebar/index.tsx
@@ -1,7 +1,98 @@
-import React from 'react'
+import { House } from 'lucide-react';
+import { useEffect, useRef, useState } from 'react';
+import styled from "styled-components";
+
+const MENU_DASHBOARD = [
+    { id: 0, icon: <House />, text: 'Bảng điều khiển' },
+    { id: 1, icon: <House />, text: 'Quản lí người dùng' },
+    { id: 2, icon: <House />, text: 'Quản lí vị trí' },
+    { id: 3, icon: <House />, text: 'Quản lí tin phòng' },
+    { id: 4, icon: <House />, text: 'Quản lí đặt phòng' },
+    { id: 5, icon: <House />, text: 'Quản lí bình luận' }
+]
+
 
 export default function Sidebar() {
+    const itemMenuRef = useRef<HTMLDivElement>(null);
+    const itemMenuRef = useRef<HTMLDivElement>(null);
+    const effRef = useRef<HTMLDivElement>(null);
+
+    const [itemMenuHeight, setItemMenuHeight] = useState<number | null>(null);
+    const [transformY, setTransformY] = useState<number>(0);
+    const [zxc, setZxc] = useState<number>(0);
+
+    const SidebarEff = styled.div`
+        display: flex;
+        align-items: center;
+        background-color: #fff;
+        border-radius: 999px 0 0 999px;
+        position: absolute;
+        top: 0;
+        left: 0;
+        right:0;
+        z-index:-1;
+        transition: transform .3s ease;
+        &::before{
+            content: "";
+            position: absolute;
+            top: -20px;
+            right: 0;
+            width: 20px;
+            height: 20px;
+            border-bottom-right-radius: 20px;
+            box-shadow: 6px 6px 0 5px #fff;
+            background-color: transparent;
+            z-index: -1;
+            pointer-events: none;
+        }
+        &::after{
+            content: "";
+            position: absolute;
+            bottom: -20px;
+            right: 0;
+            width: 20px;
+            height: 20px;
+            border-top-right-radius: 20px;
+            box-shadow: 6px -6px 0 5px #fff;
+            background-color: transparent;
+            z-index: -1;
+            pointer-events: none;
+        }
+    `;
+
+    useEffect(() => {
+        if (itemMenuRef.current) {
+            setItemMenuHeight(itemMenuRef.current.clientHeight)
+        }
+    }, []);
+
+    const handleEff = (id: number) => {
+        if (itemMenuHeight) {
+            setTransformY(itemMenuHeight * id)
+        }
+    }
+
     return (
-        <div>Sidebar</div>
+        <div className="block h-screen mt-30 pl-3">
+            {/* logo */}
+            <div className="block relative z-0">
+                <SidebarEff ref={effRef} style={{ transform: `translateY(${itemMenuHeight && itemMenuHeight * zxc}px)`, height: `${itemMenuHeight}px` }}>
+                    <span className="block bg-pink-200 size-12 rounded-full ml-1" />
+                </SidebarEff>
+                {
+                    MENU_DASHBOARD.map((item) => {
+                        const isActive = '';
+                        return (<div key={item.id} className={isActive} ref={itemMenuRef} onClick={() => setZxc(item.id)}>
+                            <p className='flex items-center gap-3 text-white pl-1 py-1 cursor-pointer'>
+                                <span className="size-12 rounded-full flex items-center justify-center">
+                                    {item.icon}
+                                </span>
+                                <span className="font-medium text-lg">{item.text}</span>
+                            </p>
+                        </div>)
+                    })
+                }
+            </div>
+        </div>
     )
 }
